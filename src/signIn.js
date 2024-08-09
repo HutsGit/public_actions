8344347687e595a79c8de911b3733ccc57a65952
hut <hut@isyscore.com>
2024-08-09 09:41:14 +0800

    1

--- src/signIn.js
+++ src/signIn.js
@@ -7,7 +7,7 @@
 const fetch = require('node-fetch');
 const { headers } = require('./config');
 
-async function sign_in() {
+async function sign_in () {
   // 查询今日是否已经签到
   const today_status = await fetch('https://api.juejin.cn/growth_api/v1/get_today_status', {
     headers,
@@ -19,7 +19,7 @@ async function sign_in() {
   if (today_status.data) return '今日已经签到！';
 
   // 签到
-  const res = await fetch('https://api.juejin.cn/growth_api/v1/check_in', {
+  const res = await fetch('https://api.juejin.cn/growth_api/v1/check_in?aid=2608&uuid=7300789319147947535&spider=0&msToken=W4WcvUJPGTsmafxqAHRtkRI3vsSqpredgCOncsJ802_a4QKnTlx-9o-Xh_ZAAQKTUuaW5B4XwlpbUXEqKuh_2NZPArEBS94d5Is5suCEZj_TNbF8P81C5ZZWWgiyZL0%3D&a_bogus=mf4OkO2uMsm1qwVfYwDz9jjExf80YW4vgZENeTuIsUw9', {
     headers,
     method: 'POST',
     credentials: 'include'
